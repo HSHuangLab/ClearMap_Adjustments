536f9ca5a3302ed868e906300d7b21f584c7a6cc
Dani Dubok <danidubok@Danis-MacBook-Neo.local>
2026-09-18 09:56:52 +0800

    Translation Example

--- examples/affine_example/create_example.py
+++ examples/affine_example/create_example.py
@@ -1,7 +1,29 @@
 
 from examples.example_utils import load_synthetic_stack
+from scipy.ndimage import affine_transform
+from examples.example_utils import plot_volume_3d
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 fixed= load_synthetic_stack()
-print("shape is:", fixed.shape)
+
+#Rotating
+center =(np.array(fixed.shape)-1)/2
+angle_degrees= 12
+angle_rad= np.deg2rad(angle_degrees)
+cos=np.cos(angle_rad)
+sin=np.sin(angle_rad)
+
+#rotating angle_degrees around x and y while z is fixed
+rotation = np.array([
+    [1,  0,  0],
+    [0,  cos,  sin],
+    [0, -sin,  cos],
+])
+
+print(rotation)
+
+
 
 
